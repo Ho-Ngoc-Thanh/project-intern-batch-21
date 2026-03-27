2c1e4f0b3d06a70a5afb5fff91f308cff9d5f3f0
tiendungv03 <votiendung2209@gmail.com>
2026-03-27 11:40:49 +0700

    feat(CRM-32): implement login UI with layout and assets

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  submitted = false;
+  showPassword = false;
+  loginForm!: FormGroup;
 
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  getUsernameErrorMessage(): string {
+    const usernameControl = this.username;
+
+    if (
+      !usernameControl ||
+      !(
+        usernameControl?.touched ||
+        usernameControl?.dirty ||
+        this.submitted === true
+      )
+    )
+      return '';
+
+    if (usernameControl?.hasError('required')) return 'Username is required';
+
+    return '';
+  }
+  getPassErrorMessage(): string {
+    const passControl = this.username;
+
+    if (
+      !passControl ||
+      !(passControl?.touched || passControl?.dirty || this.submitted === true)
+    )
+      return '';
+
+    if (passControl?.hasError('required')) return 'Password is required';
+
+    return '';
+  }
+
+  handleVerfitication() {
+    this.getUsernameErrorMessage;
+    this.getPassErrorMessage;
+  }
+
+  handleSubmit() {
+    this.submitted = true;
+    // dataLogin = {};
+    console.log('Submitted', this.loginForm);
+    // this.handleVerfitication;
+    this.router.navigate(['/']);
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 }
