a4492b3c68abf479c17ff5d749a4e5c7f9feeff2
tiendungv03 <votiendung2209@gmail.com>
2026-04-03 15:01:30 +0700

    feat(dashboard): add Featured Properties component to dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FeaturedPropertiesComponent } from './components/featured-properties/featured-properties.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FeaturedPropertiesComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
