90872c81cbe4b8e79f993c1563b97dd2f25ceb49
Bui Thi Dieu <buithidieu2004gl@gmail.com>
2026-05-05 16:21:27 +0700

    fix page not rendering and move lead management into main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component'
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
+
 export const routes: Routes = [
   {
     path: '',
@@ -20,16 +21,23 @@ export const routes: Routes = [
             (c) => c.DashboardComponent,
           ),
       },
+
+      
+      {
+        path: 'lead-management',
+        loadComponent: () =>
+          import('./features/lead-management/lead-management.component').then(
+            (c) => c.LeadManagementComponent
+          ),
+      },
+
+      
+
+      
     ],
     // canActivate:[authGuard]
   },
-  {
-  path: 'lead-management',
-  loadComponent: () =>
-    import('./features/lead-management/lead-management.component').then(
-      (c) => c.LeadManagementComponent
-    ),
-},
+
   {
     path: '',
     component: AuthLayoutComponent,
